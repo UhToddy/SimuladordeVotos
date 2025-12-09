c2e88f6f25d5e9c41f8e0467bd9863d0977bab16
MateusSouzaes <mateus.souza@estudante.ifro.edu.br>
2025-12-08 22:18:29 -0400

    FormVotação_Interativa

--- Fornularios/FormVotacao.Designer.cs
+++ Fornularios/FormVotacao.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btSalvar = new System.Windows.Forms.Button();
+            this.btConfirmar = new System.Windows.Forms.Button();
             this.btCancelar = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -36,7 +36,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNumero = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtgCandidatos = new System.Windows.Forms.DataGridView();
             this.txtNomeEscolhido = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -44,22 +44,23 @@
             this.label9 = new System.Windows.Forms.Label();
             this.txtNumEscolhido = new System.Windows.Forms.TextBox();
             this.txtPartido = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btApurar = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCandidatos)).BeginInit();
             this.SuspendLayout();
             // 
-            // btSalvar
+            // btConfirmar
             // 
-            this.btSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.btSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btSalvar.Location = new System.Drawing.Point(38, 118);
-            this.btSalvar.Margin = new System.Windows.Forms.Padding(2);
-            this.btSalvar.Name = "btSalvar";
-            this.btSalvar.Size = new System.Drawing.Size(167, 38);
-            this.btSalvar.TabIndex = 21;
-            this.btSalvar.Text = "CONFIRMAR";
-            this.btSalvar.UseVisualStyleBackColor = false;
+            this.btConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btConfirmar.Location = new System.Drawing.Point(38, 118);
+            this.btConfirmar.Margin = new System.Windows.Forms.Padding(2);
+            this.btConfirmar.Name = "btConfirmar";
+            this.btConfirmar.Size = new System.Drawing.Size(167, 38);
+            this.btConfirmar.TabIndex = 21;
+            this.btConfirmar.Text = "CONFIRMAR";
+            this.btConfirmar.UseVisualStyleBackColor = false;
             // 
             // btCancelar
             // 
@@ -109,7 +110,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(270, 9);
+            this.label2.Location = new System.Drawing.Point(360, 8);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(252, 31);
@@ -135,13 +136,13 @@
             this.txtNumero.Size = new System.Drawing.Size(83, 20);
             this.txtNumero.TabIndex = 11;
             // 
-            // dataGridView1
+            // dtgCandidatos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 172);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(426, 376);
-            this.dataGridView1.TabIndex = 22;
+            this.dtgCandidatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCandidatos.Location = new System.Drawing.Point(40, 172);
+            this.dtgCandidatos.Name = "dtgCandidatos";
+            this.dtgCandidatos.Size = new System.Drawing.Size(426, 376);
+            this.dtgCandidatos.TabIndex = 22;
             // 
             // txtNomeEscolhido
             // 
@@ -218,44 +219,55 @@
             this.txtPartido.TabIndex = 28;
             this.txtPartido.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // button1
+            // btApurar
             // 
-            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(719, 510);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 38);
-            this.button1.TabIndex = 29;
-            this.button1.Text = "APURAR";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btApurar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btApurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btApurar.Location = new System.Drawing.Point(719, 510);
+            this.btApurar.Margin = new System.Windows.Forms.Padding(2);
+            this.btApurar.Name = "btApurar";
+            this.btApurar.Size = new System.Drawing.Size(167, 38);
+            this.btApurar.TabIndex = 29;
+            this.btApurar.Text = "APURAR";
+            this.btApurar.UseVisualStyleBackColor = false;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(484, 311);
+            this.label10.Location = new System.Drawing.Point(484, 340);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(402, 25);
             this.label10.TabIndex = 30;
             this.label10.Text = "VOTO COMPUTADO COM SUCESSO";
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(548, 39);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 25);
+            this.label11.TabIndex = 31;
+            // 
             // FormVotacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(945, 588);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btApurar);
             this.Controls.Add(this.txtPartido);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.txtNumEscolhido);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.dtgCandidatos);
+            this.Controls.Add(this.btConfirmar);
             this.Controls.Add(this.btCancelar);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -266,7 +278,7 @@
             this.Controls.Add(this.txtNumero);
             this.Name = "FormVotacao";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCandidatos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,7 +286,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btConfirmar;
         private System.Windows.Forms.Button btCancelar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -282,7 +294,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNumero;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgCandidatos;
         private System.Windows.Forms.TextBox txtNomeEscolhido;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
@@ -290,7 +302,8 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox txtNumEscolhido;
         private System.Windows.Forms.TextBox txtPartido;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btApurar;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
